a14c278e3ea29f4b1f2f82c4e29cdb73eb734aa5
Paras Goel <65220753+ParasGoel1234@users.noreply.github.com>
2025-06-02 12:05:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "demo-eks-cluster"
+}
